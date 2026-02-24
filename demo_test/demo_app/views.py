7eb881ee4be2ff264a19070523296ff161084046
judetsuki <Luka9609@gmail.com>
2026-02-24 11:11:12 +0300

    adding views

--- demo_test/demo_app/views.py
+++ demo_test/demo_app/views.py
@@ -7,6 +7,18 @@ from django.urls import reverse_lazy
 # Create your views here.
 
 
+class AdminOnlyMixin(UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        return user.groups.filter(name__in=['Admin']).exists()
+
+
+class StaffOnlyMixin(UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        return user.groups.filter(name__in=['Admin', 'Manager']).exists()
+
+
 class UserLoginView(LoginView):
     template_name = 'login.html'
 
@@ -22,25 +34,53 @@ class UserListView(ListView):
 class ViewProduct(ListView):
     model = Product
     context_object_name = 'products'
-    
+    template_name = 'product_list.html'
 
-class CreateProduct(UserPassesTestMixin, CreateView): 
+    def get_queryset(self):
+        user = self.request.user
+        is_staff = user.is_authenticated and user.groups.filter(name__in=['Admin', 'Manager']).exists()
+
+        if is_staff:
+            sort = self.request.GET.get('sort')
+            if sort:
+                queryset = queryset.order_by(sort)
+
+            search = self.request.GET.get('search')
+            if search:
+                queryset = queryset.filter(name__icontains=search)
+        return queryset
+
+
+class CreateProduct(AdminOnlyMixin, CreateView): 
     model = Product
     fields = '__all__'
     template_name = 'product_form.html'
     success_url = reverse_lazy
 
-    def test_func(self):
-        user = self.request.user
-        return user.groups.filter(name__in=['Admin']).exists()
 
-class UpdateProduct(UpdateView):
+class UpdateProduct(AdminOnlyMixin, UpdateView):
 
 
-class DeleteProduct(DeleteView):
+class DeleteProduct(AdminOnlyMixin, DeleteView):
 
 
 class ViewOrder(ListView):
+    model = Order
+    context_object_name = 'Orders'
+
+    def get_queryset(self):
+        user = self.request.user
+        is_staff = user.is_authenticated and user.groups.filter(name__in=['Admin', 'Manager']).exists()
+
+        if is_staff:
+            sort = self.request.GET.get('sort')
+            if sort:
+                queryset = queryset.order_by(sort)
+
+            search = self.request.GET.get('search')
+            if search:
+                queryset = queryset.filter(name__icontains=search)
+        return queryset
 
 
 
