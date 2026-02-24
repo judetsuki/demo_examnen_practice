311046bcd829be2487d80453f7a063ea937f27c5
judetsuki <Luka9609@gmail.com>
2026-02-24 09:59:51 +0300

    first commit

---
+++ demo_test/demo_app/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey('manufacturer', on_delete=models.CASCADE)
+    category =
+    discount =
+    stock =
+    description =
+
+
+class Order(models.Model):
+    STATUS_CHOICES =[
+        ('completed', 'завершен'),
+        ('new', 'новый')
+    ]
+    order_date
+    delivery_date = 
+    status = delivery_address =
+    receiver_name = 
+    delivery_code = 
+    user =
+
+class OrderDetails(models.Model):
+    order =
+    product =
+    order_quantity =
+
+
+class Supplier(models.Model):
+name
+
+class Category(models.Model):
+name
+
+class Manufacturer(models.Model):
+name
+
+class DeliveryPoint(models.Model):
+    address
